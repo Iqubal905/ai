23d3be28a2318afa5a40f1298bd948b1e989b079
iqubal905 <iqubal905@gmail.com>
2023-03-03 10:26:23 +0600

    modalDesign

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const dataDisplayed = (technologies) => {
 const containerSection = document.getElementById('container')
 containerSection.innerHTML = ''
 technologies.forEach(technology => {
-    console.log(technology);
+   // console.log(technology);
  const div = document.createElement("div");
  div.innerHTML = `
  <div class="card  w-full text-black bg-white border border-lime-400 ">
@@ -30,7 +30,8 @@ technologies.forEach(technology => {
      <h1>${technology.name}</h1>
     <span> <i class="fa-solid fa-calendar-days"></i> ${technology.published_in}</span>
      </div>
-     <i class="fa-solid fa-arrow-right mt-8"></i>
+     <label onclick="dataLoadSingleTechnology()" for="my-modal-3"><i  class="fa-solid fa-arrow-right mt-8"></i></label>
+     
     </div>
 
  </div>
@@ -42,7 +43,7 @@ technologies.forEach(technology => {
     
 }
 
-dataLoad()
+
 
 // show all data
 
@@ -53,3 +54,32 @@ const showAlltechonology = () => {
     .then(data => dataDisplayed(data.data.tools))
    
 }
+
+// get and show single info of technology in modal
+
+const dataLoadSingleTechnology = () =>{
+    fetch('https://openapi.programming-hero.com/api/ai/tool/01')
+    .then(res => res.json())
+    .then(data => displaySingleTechnologyModal(data))
+}
+
+const displaySingleTechnologyModal = (tech) => {
+  const modalContainer = document.getElementById('modal-info')
+  const div = document.createElement("div");
+  div.classList.add("modal");
+  div.innerHTML = `
+  <div class="modal-box relative">
+    <label for="my-modal-3" class="btn btn-sm btn-circle absolute right-2 top-2">✕</label>
+    <h3 class="text-lg font-bold">Congratulations random Internet user!</h3>
+    <p class="py-4">You've been selected for a chance to get one year of subscription to use Wikipedia for free!</p>
+  </div>
+  `
+  modalContainer.appendChild(div)
+}
+
+
+
+
+
+
+dataLoad()
